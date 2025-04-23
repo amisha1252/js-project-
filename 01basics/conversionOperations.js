9ad454ebcab57716b885ed6a7d65d99a6896a8b5
Amisha  chhabra <124507963+amisha1252@users.noreply.github.com>
2025-04-23 13:54:16 +0000

    operations and datatypes convrsion

--- 01basics/conversionOperations.js
+++ 01basics/conversionOperations.js
@@ -91,7 +91,50 @@
 
 // string Conversion
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//   *********************operations*****************
+
+// let value =3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);//power
+// console.log(2%2);//mod
+
+
+// let str1 = "Hello";
+// let str2 = " Amisha";
+
+// let str3 = str1+ str2;
+// console.log(str3);
+
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log(+true);//1
+// console.log(true+);//****error****
+// console.log(+true+);//error
+// console.log(+"");//0
+
+
+// let num1 , num2 ,num3
+// num1= num2=  num3 = 2+2 //bad prac for readablity
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
